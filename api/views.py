d69b8998f918babe46bd1f9b42f86928d172918a
unknown <Abhishek>
2020-11-25 21:09:54 +0530

    APIVIEW

---
+++ api/views.py
@@ -0,0 +1,67 @@
+from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
+
+from .serializers import StudentSerializer
+from .models import Student
+from rest_framework.parsers import JSONParser
+from rest_framework.renderers import JSONRenderer
+from django.http import HttpResponse, JsonResponse
+from django.utils.decorators import method_decorator
+import io
+from django.views import View
+
+
+# Create your views here.
+@method_decorator(csrf_exempt, name='dispatch')
+class StudentAPI(View):
+    def get(self, request, *args, **kwargs):
+        json_data = request.body
+        stream = io.BytesIO(json_data)
+        pythondata = JSONParser().parse(stream)
+        id = pythondata.get('id', None)
+        if id is not None:
+            stu = Student.objects.get(id=id)
+            serializer = StudentSerializer(stu)
+            json_data = JSONRenderer().render(serializer.data)
+            return HttpResponse(json_data, content_type='application/json')
+
+        stu = Student.objects.all()
+        serializer = StudentSerializer(stu, many=True)
+        return JsonResponse(serializer.data, safe=False)
+
+
+    def post(self, request, *args, **kwargs):
+        json_data = request.body
+        stream = io.BytesIO(json_data)
+        pythondata = JSONParser().parse(stream)
+        serializer = StudentSerializer(data=pythondata)
+        if serializer.is_valid():
+            serializer.save()
+            res = {'msg': 'Data Created'}
+            return JsonResponse(res, safe=False)
+
+        return JsonResponse(serializer.errors, safe=False)
+
+    def put(self, request, *args, **kwargs):
+        json_data = request.body
+        stream = io.BytesIO(json_data)
+        pythondata = JSONParser().parse(stream)
+        id = pythondata.get('id')
+        stu = Student.objects.get(id=id)
+        serializer = StudentSerializer(stu, data=pythondata, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            res = {'msg': 'Data Updated!!'}
+            return JsonResponse(res, safe=False)
+
+        return JsonResponse(serializer.errors, safe=False)
+
+    def delete(self, request, *args, **kwargs):
+        json_data = request.body
+        stream = io.BytesIO(json_data)
+        pythondata = JSONParser().parse(stream)
+        id = pythondata.get('id')
+        stu = Student.objects.get(id=id)
+        stu.delete()
+        res = {'msg': 'Data Deleted !!'}
+        return JsonResponse(res, safe=False)
---
+++ myapp.py
@@ -0,0 +1,60 @@
+import requests
+import json
+
+URL = "http://127.0.0.1:8000/stu/"
+
+
+def get_data(id=None):
+    data = {}
+    if id is not None:
+        data = {'id': id}
+    json_data = json.dumps(data)
+    r = requests.get(url=URL, data=json_data)
+    data = r.json()
+    print(data)
+
+
+# get_data()
+
+
+def post_data():
+    data = {
+        'name': 'Dinesh',
+        'roll': 456,
+        'city': 'Banglore'
+    }
+    json_data = json.dumps(data)
+    r = requests.post(url=URL, data=json_data)
+    data = r.json()
+    print(data)
+
+
+# post_data()
+
+
+def update_data():
+    data = {
+        'id': 5,
+        'name': 'Dinesh',
+        'roll': 457,
+        'city': 'Rewa'
+    }
+    json_data = json.dumps(data)
+    r = requests.put(url=URL, data=json_data)
+    data = r.json()
+    print(data)
+
+
+# update_data()
+
+
+def delete_data():
+    data = {'id': 5}
+
+    json_data = json.dumps(data)
+    r = requests.delete(url=URL, data=json_data)
+    data = r.json()
+    print(data)
+
+
+delete_data()
